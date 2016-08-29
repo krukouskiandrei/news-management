baa7d12027694754ad8f2fa2fb8c7c551c5a5bf1
Krukouski Andrei <andrei_krukouski@epam.com>
2016-08-29 14:45:10 +0300

    delete securityConfig.java

--- news-admin/src/main/java/com/epam/newsmanagement/admin/utils/SecurityWebInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.epam.newsmanagement.admin.utils;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- *
- */
-public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
-}
